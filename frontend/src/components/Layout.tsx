17e240e07a96020eb1805e4e6de84c3cd0947d30
Nirvan Jain <jainnirvan070@gmail.com>
2026-08-11 09:56:23 +0530

    update

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -3,16 +3,16 @@ import Sidebar from './Sidebar'
 
 export default function Layout() {
   return (
-    <div className="flex min-h-screen max-w-[1200px] mx-auto">
-      {/* Left Sidebar */}
-      <aside className="w-64 flex-shrink-0 border-r border-[#2f3336] hidden md:block">
-        <Sidebar />
-      </aside>
+      <div className="flex min-h-screen max-w-[1200px] mx-auto">
+        {/* Left Sidebar */}
+        <aside className="w-64 flex-shrink-0 border-r border-[#2f3336] hidden md:block">
+          <Sidebar />
+        </aside>
 
-      {/* Main Content */}
-      <main className="flex-1 min-h-screen border-r border-[#2f3336]">
-        <Outlet />
-      </main>
-    </div>
+        {/* Main Content */}
+        <main className="flex-1 min-h-screen border-r border-[#2f3336]">
+      <Outlet />
+    </main>
+  </div>
   )
 }
